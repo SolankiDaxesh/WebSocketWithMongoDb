585ee6d52ac46b9cdaa68cf84146e0d785c5bdbf
solan <solan@SOLANKIDAXESH>
2024-11-01 09:29:16 +0530

    Add project files.

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace WebSocket_Demo.Controllers
+{
+    [Route("ws/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        [HttpGet("connect")]
+        public async Task Connect()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                await HandleWebSocketConnection(webSocket);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = 400;
+            }
+        }
+
+        private async Task HandleWebSocketConnection(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed by client", CancellationToken.None);
+                }
+                else
+                {
+                    var receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+                    // Process the received message (e.g., location update)
+                    Console.WriteLine($"Received: {receivedMessage}");
+
+                    // Here, you might broadcast the location update to other WebSocket clients.
+                    // For example:
+                    var responseMessage = Encoding.UTF8.GetBytes("Location update received");
+                    await webSocket.SendAsync(new ArraySegment<byte>(responseMessage), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+            }
+        }
+    }
+}
